5b8cd0afec7e4e350fcafe0f5e6facab8f533971
Charles Allison <soprinye@gmail.com>
2022-09-23 18:48:07 +0100

    adds health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	handler "github.com/charlallison/go-rest-api/internal/transport/http"
 )
 
 // App - struct that contains things like pointers to
@@ -11,6 +14,14 @@ type App struct{}
 // Run - sets up our application
 func (app *App) Run() error {
 	fmt.Println(`Setting up our APP`)
+
+	h := handler.NewHandler()
+	h.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", h.Router); err != nil {
+		fmt.Println(`Failed to setup server`)
+	}
+
 	return nil
 }
 
